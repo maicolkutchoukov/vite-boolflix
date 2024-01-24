e845d820a356fbd98d4cca665f73c559272c47f3
maicolkutchoukov <maicol.j.95@gmail.com>
2024-01-24 13:09:21 +0100

    Inizializzato progetto Boolflix

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,11 @@ import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 import AppFooter from './components/AppFooter.vue';
 import axios from 'axios';
+import { store } from './store.js';
 export default {
     data() {
         return {
-
+            store
         };
     },
     components: {
